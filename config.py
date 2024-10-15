360b11828683fa990514a2cc7305407ad1b4225a
shahafbr <shahafbrenner@gmail.com>
2024-10-15 14:21:10 +0200

    Adding home page and fixing configurations

--- config.py
+++ config.py
@@ -1,21 +1,12 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-class Config(object):
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+class Config(object): 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = False
 
 class LocalConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    # dbuser=os.getenv('DBUSER'),
-    # dbpass=os.getenv('DBPASS'),
-    # dbhost=os.getenv('DBHOST'),
-    # dbname=os.getenv('DBNAME')
-    # )
-    # DEBUG = True
+    DEBUG = True
 
 class GithubCIConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
